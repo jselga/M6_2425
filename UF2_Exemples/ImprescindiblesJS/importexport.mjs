e5f397a3f7687b4c494762f65e33aa337b3778a0
jselga <jordi.selga@iescarelesvallbona.cat>
2024-11-19 20:35:03 +0100

    ImprescindiblesJS part modular

---
+++ UF2_Exemples/ImprescindiblesJS/importexport.mjs
@@ -0,0 +1,21 @@
+const chanchosFelices =['chancho1','chancho2','chancho 3']
+// export const chanchosTristes =['chanchot1','chanchot2','chanchot3']
+// export const otrosChanchos =[]
+const chanchosTristes =['chanchotr1','chanchotr2','chanchotr3']
+const otrosChanchos =['no hay otros']
+
+// module.exports = {
+//     chanchosFelices,
+//     chanchosTristes
+// }
+const fn1 = ()=>{
+    console.log('soy la funcion 1');
+    
+}
+const fn2 = ()=>{
+    console.log('soy la funcion 2');
+    
+}
+// Otra forma
+export {chanchosTristes,otrosChanchos,fn1,fn2}
+export default chanchosFelices
