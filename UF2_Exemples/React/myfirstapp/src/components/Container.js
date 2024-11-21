c54a18a68d1f7c2ed1f30f3ff37f41ddbab86597
jselga <jordi.selga@iescarelesvallbona.cat>
2024-11-21 12:43:37 +0100

    Canviant el títol fent servir useState

--- UF2_Exemples/React/myfirstapp/src/components/Container.js
+++ UF2_Exemples/React/myfirstapp/src/components/Container.js
@@ -4,9 +4,16 @@ import { Llista } from "./Llista";
 export function Container(){
     let [titol, setTitol] = useState("Titol Llista");
     let [dades, setDades] = useState(["John Doe", "Pepe", "manel"]);
+    function canviTitol() {
+        let tt = prompt("Nou titol:")
+        if (tt) {
+          setTitol(tt);
+        //document.getElementById("titol").innerHTML = tt;
+      }
+      }
     return(
         <>
-        <h1>{titol}</h1>
+        <h1 onClick={canviTitol}>{titol}</h1>
         <div>
             <Llista data={dades} />
         </div>
