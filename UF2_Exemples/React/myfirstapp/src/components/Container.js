449864eb9684726184d463935039a8bdd3a9b237
jselga <jordi.selga@iescarelesvallbona.cat>
2024-11-21 12:46:26 +0100

    Afegint elements

--- UF2_Exemples/React/myfirstapp/src/components/Container.js
+++ UF2_Exemples/React/myfirstapp/src/components/Container.js
@@ -4,6 +4,13 @@ import { Llista } from "./Llista";
 export function Container(){
     let [titol, setTitol] = useState("Titol Llista");
     let [dades, setDades] = useState(["John Doe", "Pepe", "manel"]);
+    function addElem() {
+        let elem = prompt("Nou item de la llista");
+        if (elem) {
+          dades.push(elem);
+          setDades([...dades]);
+        }
+      }
     function canviTitol() {
         let tt = prompt("Nou titol:")
         if (tt) {
@@ -16,6 +23,7 @@ export function Container(){
         <h1 onClick={canviTitol}>{titol}</h1>
         <div>
             <Llista data={dades} />
+            <button onClick={addElem}>Afegir</button>
         </div>
         </>
     )
