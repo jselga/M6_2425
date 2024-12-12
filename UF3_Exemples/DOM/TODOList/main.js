5d7597089f5bd5f8561dccbde8dcee72d60be0a1
jselga <jordi.selga@iescarelesvallbona.cat>
2024-12-12 14:04:57 +0100

    Obtenint valors d'un formulari

--- UF3_Exemples/DOM/TODOList/main.js
+++ UF3_Exemples/DOM/TODOList/main.js
@@ -1,9 +1,14 @@
 window.onload=()=>  {
-    console.log('intro a DOM desde main.js');
-    const parrafo = document.getElementById("text");
-    console.log(parrafo.innerHTML);
-    parrafo.innerText='Texto actualizado'
-    parrafo.innerHTML='<ul><li>Elemento 1</li><li>Elemento 2</li></ul>'
+const form = document.getElementById('todo-form');
+form.onsubmit = (e)=>{
+    //Hem d'afegir prevent default per prevenir que la nostra aplicació es refresqui
+    e.preventDefault();
+    const todo = document.getElementById('todo');
+    const todoText = todo.value;
+    todo.value ='';
+    console.log(todoText);
+    
+}
 }
 
 
