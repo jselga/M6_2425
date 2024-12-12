4a876f0284722f20e97e54c9fd40ceba5a280e08
jselga <jordi.selga@iescarelesvallbona.cat>
2024-12-12 17:27:41 +0100

    Funció recursiva render

--- UF3_Exemples/DOM/TODOList/main.js
+++ UF3_Exemples/DOM/TODOList/main.js
@@ -1,38 +1,43 @@
-const todos=[];
+const todos = [];
 
-window.onload=()=>  {
-const form = document.getElementById('todo-form');
-form.onsubmit = (e)=>{
-    //Hem d'afegir prevent default per prevenir que la nostra aplicació es refresqui
-    e.preventDefault();
-    const todo = document.getElementById('todo');
-    const todoText = todo.value;
-    todo.value ='';
-    todos.push(todoText);
+const render = () => {
     const todoList = document.getElementById('todo-list');
-    const todosTemplate = todos.map( element=> '<li>' + element+'</li>');
+    const todosTemplate = todos.map(element => '<li>' + element + '</li>');
     console.log(todosTemplate);
     todoList.innerHTML = todosTemplate.join('');
     const elements = document.querySelectorAll('#todo-list li')
     // Array.from(elements);
     // elements.forEach(x=>console.log(x));
-    elements.forEach((element,index)=>{
-        element.addEventListener('click',()=>{
+    elements.forEach((element, index) => {
+        element.addEventListener('click', () => {
             //eliminem del DOM
             element.parentNode.removeChild(element);
             // eliminem de l'array
-            todos.splice(index,1);
-            console.log(todos,index);
+            todos.splice(index, 1);
+            render()
+            console.log(todos, index);
         })
-        
-        
-    })
-    
-    
 
-    
-    
+
+    })
 }
+window.onload = () => {
+    const form = document.getElementById('todo-form');
+    form.onsubmit = (e) => {
+        //Hem d'afegir prevent default per prevenir que la nostra aplicació es refresqui
+        e.preventDefault();
+        const todo = document.getElementById('todo');
+        const todoText = todo.value;
+        todo.value = '';
+        todos.push(todoText);
+        render()
+
+
+
+
+
+
+    }
 }
 
 
